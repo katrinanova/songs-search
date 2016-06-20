d97caf885ba48a2666a9da2b70bd93912f57b64e
Katrina Nova <katrina.a.nova@gmail.com>
2016-06-19 23:02:29 -0400

    implement test for searches controller

--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class SearchesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "shoul get home" do
+    get(:home)
+    assert_select "h3", "Please select parameters for search"
+  end
+
+  test "should find both songs by Drake" do
+    get(:search, {"artist_name"=>"dr"})
+    assert_select "li", 2
+    assert_select "p", "Song Title"
+    assert_select "p", "Song Title2"
+
+  end
+
+  test "should only show one song that matches song parameter" do
+    get(:search, {"song_title"=>"2"})
+    assert_select "li", 1
+    assert_select "p", "Song Title2"
+  end
+
+  test "should not show any sogs if no match" do
+    get(:search, {"album_title"=>"fdasf"})
+    assert_select "li", 0
+    assert_select "h3", "Sorry, no songs match those parameters"
+  end
 end

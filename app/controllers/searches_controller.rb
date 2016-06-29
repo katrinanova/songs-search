5d2338343213222c2760f1efb060ab0d105516fa
Katrina Nova <katrina.a.nova@gmail.com>
2016-06-29 17:17:00 -0400

    create no_song_message view

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,6 +2,12 @@ class SearchesController < ApplicationController
 
   def search
     @search = Search.new(params)
+
+    if @search.results.empty?
+      render :no_songs_message
+    else
+      render :search_results
+    end
   end
 
 end
